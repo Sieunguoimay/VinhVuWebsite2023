453289d185343c5651db662c611afba961dd47b4
DESKTOP-KFPOJAK\ADMIN <vuduydu1997@gmail.com>
2023-06-03 17:14:51 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainComponent from "./components/MainComponent.vue"
+</script>
+
+<template>
+  <main>
+    <MainComponent/>
+  </main>
+</template>
+
+<style scoped>
+</style>
